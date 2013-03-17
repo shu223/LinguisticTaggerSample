27c0d87db8736c423760c70d468a74ea5e54a3aa
shu223 <shuichi0526@gmail.com>
2013-03-18 02:49:34 +0900

    Initial Commit

---
+++ LinguisticTagger/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LinguisticTagger
+//
+//  Created by shuichi on 13/03/18.
+//  Copyright (c) 2013年 Shuichi Tsutsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LinguisticTagger/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LinguisticTagger
+//
+//  Created by shuichi on 13/03/18.
+//  Copyright (c) 2013年 Shuichi Tsutsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
